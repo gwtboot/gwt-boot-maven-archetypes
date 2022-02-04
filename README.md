878a7e52cb5a6c1501511cf5f59136e559fa353c
lofidewanto <lofidewanto@gmail.com>
2022-02-04 18:44:44 +0100

    Initial import

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
-# gwt-boot-maven-archetypes
-Maven Archetypes for GWT Boot
+# GWT Boot Maven Archetypes
+Maven Archetypes for GWT Boot projects. You can generate all the GWT Boot projects with this Maven Archetypes.
+
+To run the archetypes:
+
+```
+mvn archetype:generate 
+  -DarchetypeGroupId=com.github.gwtboot 
+  -DarchetypeArtifactId="gwt-boot-artifactId" 
+  -DarchetypeVersion=[Version Number] 
+  -DgroupId="my-groupid" 
+  -DartifactId="my-artifactId"
+  -DprojectName="my-project-name"
+  -DprojectDescription="my-project-description"
+```
+
+Example:
+```
+mvn archetype:generate -DarchetypeGroupId=com.github.gwtboot -DarchetypeArtifactId=gwt-boot-basic-archetype 
+```
+
+where:
+ * **"my-groupid"** == my newly created KissMDA app Maven group
+ * **"my-artifactId"** == my newly creadted KissMDA app Maven artifact
+ * **"my-project-name"** == the name of my project
+ * **"my-project-description"** == the description of my project
+
+ # Possible Archetype for GWT Boot
+ * gwt-boot-basic-archetype
+
+ 
+
+ 
---
+++ gwt-boot-basic-archetype/src/main/resources/archetype-resources/src/main/java/client/AppEntryPoint.java
@@ -0,0 +1,46 @@
+#set( $symbol_pound = '#' )
+#set( $symbol_dollar = '$' )
+#set( $symbol_escape = '\' )
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package ${package}.client;
+
+import java.util.logging.Logger;
+
+import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.user.client.Window;
+import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.RootPanel;
+
+public class AppEntryPoint implements EntryPoint {
+
+	private static Logger logger = Logger
+			.getLogger(AppEntryPoint.class.getName());
+
+	@Override
+	public void onModuleLoad() {
+		Button button = new Button("Click me");
+		button.addClickHandler(clickEvent -> {
+			Window.alert("Hello World!");
+			logger.info("Hello World!");
+		});
+
+		RootPanel.get("helloButton").add(button);
+	}
+}
